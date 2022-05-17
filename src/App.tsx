aacda2fdd044adbf97fadf482390f4a1af2279a6
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2022-05-17 17:22:10 +0300

    1.Added functions: removeTask, AddTask. 2. Added handler of title changing. 3. added error handler when trying to add a task with an empty titleю

--- src/App.tsx
+++ src/App.tsx
@@ -18,22 +18,31 @@ function App() {
 
 
   let [tasks, setTasks] = useState<Array<TaskType>>([
-    {id: v1(), title: 'Terminator', isDone: true},
-    {id: v1(), title: 'Furminator', isDone: false},
-    {id: v1(), title: 'Gladiator', isDone: true},
-    {id: v1(), title: 'Penetrator', isDone: false},
+    { id: v1(), title: 'Terminator', isDone: true },
+    { id: v1(), title: 'Furminator', isDone: false },
+    { id: v1(), title: 'Gladiator', isDone: true },
+    { id: v1(), title: 'Penetrator', isDone: false },
   ])
 
+  const removeTask = (taskID: string) => {
+    setTasks(tasks.filter(t => t.id !== taskID))
+  }
 
+  const addTask = (newTaskTitle: string) => {
+    const newTask: TaskType = {id: v1(), title: newTaskTitle, isDone: false}
+    setTasks([newTask, ...tasks])
+  }
 
 
 
   return (
     <div className="App">
-      <TodoList 
+      <TodoList
         title={'Movies'}
-          tasks={tasks}
-        />
+        tasks={tasks}
+        removeTask={removeTask}
+        addTask={addTask}
+      />
     </div>
   );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,29 +1,58 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { TaskType } from "./App";
 
 export type TodoListPropsType = {
     tasks: Array<TaskType>
     title: string
+    removeTask: (taskID: string) => void
+    addTask: (newTaskTitle: string) => void
 }
 
 
 
 const TodoList = (props: TodoListPropsType) => {
+
+
+    let [title, setTitle] = useState('')
+    let [error, setError] = useState<string | null>(null)
+
+    const addTask = () => {
+        if (title.trim()) {
+            props.addTask(title)
+            setTitle('')
+        } else {
+            setError('Title is required')
+        }
+    }
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+        setError(null)
+    }
+
+
     return (
         <div>
             <div>
                 <h3>{props.title}</h3>
-                <input />
-                <button>+</button>
+                <input onChange={onChangeHandler} value={title} className={error ? 'error' : ''}/>
+               <button onClick={addTask}>+</button>
+                {error && <div className="error-message">{error}</div>}
             </div>
             <div>
                 {
                     props.tasks.map(t => {
+                        const removeTask = () => {
+                            props.removeTask(t.id)
+                        }
+
                         return (
                             <div key={t.id}>
-                                <input type="checkbox" checked={t.isDone}/>
+                                <input
+                                    type="checkbox"
+                                    checked={t.isDone}
+                                />
                                 <span>{t.title}</span>
-                                <button>x</button>
+                                <button onClick={removeTask}>x</button>
                             </div>
                         )
                     })
